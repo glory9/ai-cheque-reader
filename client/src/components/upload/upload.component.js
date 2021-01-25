8763c8de1a079a4abadc27672ebc8f44fc944191
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2021-01-24 21:51:53 -0500

    udpate api

--- client/src/components/upload/upload.component.js
+++ client/src/components/upload/upload.component.js
@@ -1,6 +1,34 @@
 import React, {Component} from 'react';
 
+// Image Save
+import axios from 'axios';
+
+// Image Upload
+import ImageUploading from "react-images-uploading";
+
 const upload = class upload extends Component{
+
+    onChange = (imageList) => {
+        // data for submit
+        
+        // Create an object of formData 
+        const formData = new FormData(); 
+         
+        // Update the formData object 
+        formData.append( 
+          "myFile", 
+          imageList[0].file, 
+          imageList[0].file.name
+        ); 
+
+      // Details of the uploaded file 
+      console.log(imageList[0].file); 
+   
+      // Request made to the backend api 
+      // Send formData object to my php file for save in folder
+      axios.post("http://localhost:3000/reactimageupload.php", formData); 
+    }; 
+    
     constructor(props){
         super(props);
 
@@ -11,6 +39,8 @@ const upload = class upload extends Component{
         this.setState({})
     }
 
+
+    
     render(){
         return (
             <div>
@@ -18,7 +48,30 @@ const upload = class upload extends Component{
                 <label for="formFileLg" class="form-label">Please submit check image for validation</label>
                 <input class="form-control form-control-lg" id="formFileLg" type="file"/>
                 <a href="/results" class="btn btn-success">Submit</a>
+{/* 
+testing this out                  */}
+                <ImageUploading
+                onChange={this.onChange}
+            >
+                {({ imageList, onImageUpload }) => (
+                // write your building UI
+                <div className="imageuploader">
+                    <div className="mainBtns">
+                    <button className="btn btn-primary mr-1" onClick={onImageUpload}>Upload Image</button>
+                    
+                    </div>
+                    {imageList.map((image) => (
+                    <div className="imagecontainer" key={image.key}>
+                        <img src={image.dataURL} />
+                        
+                    </div>
+                    ))}
+                </div>
+                )}
+            </ImageUploading>
+                    
             </div>
+            
         )
     }
 }
