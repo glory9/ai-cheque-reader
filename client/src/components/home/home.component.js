a155c0f0072dcd4a6c08ba6e18fa3cc19275645e
Glory Adedayo <gloryadedayo@ExpressmeXP4100.mshome.net>
2021-01-25 05:15:49 -0600

    design changes

--- client/src/components/home/home.component.js
+++ client/src/components/home/home.component.js
@@ -28,38 +28,12 @@ const home = class home extends Component{
                     </div>
                 </div>
 
-                {/* <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-                    </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="https://picsum.photos/1000/250" class="d-block w-100" alt="..."/>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://picsum.photos/700/250" class="d-block w-100" alt="..."/>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://picsum.photos/700/250" class="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </a>
-                </div> */}
-
                 <div class="card text-center" >
                     <div class="card-body">
-                        <h5 class="card-title">AI Check Reader</h5>
-                        <p class="card-text">Submit your check for validation</p>
-                        <a href="/get-started" class="btn btn-primary">Get Started/Begin New Transaction</a>
+                        <h3 class="card-title"><b>AI Check Reader</b></h3>
+                        <h4 class="card-text">Submit your check for validation</h4>
+                        <br/>
+                        <a href="/get-started" class="btn btn-primary btn-lg">Begin New Transaction</a>
                     </div>
                 </div>
             </div>
--- client/src/components/options/options.component.js
+++ client/src/components/options/options.component.js
@@ -17,7 +17,7 @@ const options = class options extends Component{
                 <br></br><br></br>
                 <a href="/upload" class="btn btn-outline-primary">Upload file</a>
                 <a href="/enterURL" class="btn btn-outline-success">Enter Image URL</a>
-                <a href="/snapshot" class="btn btn-outline-info">Take Snapshot (under development)</a>
+                <a href="/snapshot" class="btn btn-outline-info">Take Snapshot</a>
                 <br></br>
             </div>
         )
